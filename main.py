3af171f9e4c3ded56573673a9a5bfd9677d1f627
Firefret <firefret@proton.me>
2025-08-12 17:48:48 +0300

    Added constants

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from asteroids!")
+import pygame
+from constants import (SCREEN_WIDTH, SCREEN_HEIGHT,
+ASTEROID_MAX_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MIN_RADIUS)
 
+def main():
+    pygame.init()
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
