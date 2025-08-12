876d6d70ed0b653b7e4ba861c2280327fdd839dc
Firefret <firefret@proton.me>
2025-08-12 18:36:11 +0300

    Added player class and its drawing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import (SCREEN_WIDTH, SCREEN_HEIGHT,
 ASTEROID_MAX_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MIN_RADIUS)
+from player import Player
 
 def main():
     pygame.init()
@@ -9,6 +10,7 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     pygame.display.set_caption("Asteroids")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -16,8 +18,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
+
         dt = clock.tick(200)/1000
 
 if __name__ == "__main__":
